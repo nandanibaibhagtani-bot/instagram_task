588122cb7a2b8b691abb9ddfcd8d72c31709c5f4
$git_commit_name <$git_commit_email>
2026-08-20 13:51:34 +0500

    update server.js for vercel

--- instagram-backend/server.js
+++ instagram-backend/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const authRoute = require('./routes/auth'); 
+const serverless = require('serverless-http');
+const authRoute = require('./routes/auth');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRoute);
 
-app.listen(4000, () => {
-    console.log("Server running on port 4000...");
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(4000, () => {
+        console.log("Server running on port 4000...");
+    });
+}
+
+module.exports = serverless(app);
